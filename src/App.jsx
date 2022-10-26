1b26b1cac2e37b11f518288cbc5bb93638418c1a
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-27 07:41:53 +0900

    Add button to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => {
+    console.log("test");
+  };
   return (
     <>
       <h1>Hello!!!</h1>
       <p>How are you?</p>
+      <button onClick={onClickButton}>Button</button>
     </>
   );
 };
