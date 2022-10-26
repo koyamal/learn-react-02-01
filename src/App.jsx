9a155a3f7276716cdaa4b15a8d0800bb2894f9b6
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-27 08:46:34 +0900

    separation of concerns in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+
+import React, { useEffect, useState } from "react";
 
 import ColorfulMessage from "./components/ColorfulMessage";
 
@@ -12,11 +14,15 @@ const App = () => {
   const onClickShowFace = () => {
     setFaceShowFlag(!faceShowFlag);
   };
-  if (num % 3 === 0) {
-    faceShowFlag || setFaceShowFlag(true);
-  } else {
-    faceShowFlag && setFaceShowFlag(false);
-  }
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>Hello!!!</h1>
