ba1c92cef999be2f065f08b9982de087878f46eb
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-27 07:43:32 +0900

    Add style of p in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ const App = () => {
   const onClickButton = () => {
     console.log("test");
   };
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
       <h1 style={{ color: "red" }}>Hello!!!</h1>
-      <p>How are you?</p>
+      <p style={contentStyle}>How are you?</p>
       <button onClick={onClickButton}>Button</button>
     </>
   );
